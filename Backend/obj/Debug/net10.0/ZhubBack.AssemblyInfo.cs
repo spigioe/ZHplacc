f0ada9f10374702d6dc0a39480c3344b40405fc2
Spiegelhalter Dominik <spiegelhalter.dominik467@gmail.com>
2026-05-04 00:16:49 +0200

    Last stable state before creating development branch

--- Backend/obj/Debug/net10.0/ZhubBack.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/ZhubBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZhubBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf68194a6e663caac5ed5b603f125a5aa3b4ab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c37beedf40f6edb93627ebbedb07b98fc5cabc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZhubBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZhubBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
